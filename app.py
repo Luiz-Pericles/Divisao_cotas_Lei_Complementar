b28a4439891b3247d043aeb19ad40b4162a8fb37
Luiz Péricles <136658343+Luiz-Pericles@users.noreply.github.com>
2024-09-13 11:18:05 -0300

    Update 2.0 app.py

--- app.py
+++ app.py
@@ -2,9 +2,17 @@
 from classificar_itens import classificar_itens
 import pandas as pd
 import streamlit as st
+from io import BytesIO
 
 
-
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+    
 
 def main():
     st.title("Classificação de Itens para ME/EPP e Ampla Concorrência")
@@ -14,22 +22,20 @@ def main():
         "Selecione o arquivo Excel", type=["xlsx", "xls"])
     
     if uploaded_file:
-
-
-    # Leitura do arquivo Excel
         df = pd.read_excel(uploaded_file)
-
-    # Aplicar a função de classificação
         df_classificado = classificar_itens(df)
 
-    # Download do arquivo processado
+        # Mostrar o DataFrame classificado
         st.write(df_classificado)
+
+        # Criar o arquivo Excel em memória e permitir o download
+        excel_data = to_excel(df_classificado)
+
         st.download_button(
             label="Baixar Planilha Classificada",
-            data=df_classificado.to_excel(index=False, engine='xlsxwriter'),
+            data=excel_data,
             file_name="planilha_classificada.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
 
 if __name__ == '__main__':
     main()
