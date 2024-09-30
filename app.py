cd12d235c3612a222cfbceb4fc51acf8e031646e
Luiz Péricles <136658343+Luiz-Pericles@users.noreply.github.com>
2024-09-30 12:32:30 -0300

    asasasas

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 
-from classificar_itens import classificar_itens, conferir_entrada_de_dados, carregar_instrucoes, gerar_planilha_exemplo
+from classificar_itens import classificar_itens, conferir_entrada_de_dados, carregar_instrucoes
 import pandas as pd
 import streamlit as st
 from io import BytesIO
-
 
 
 def to_excel(df):
@@ -17,7 +16,7 @@ def to_excel(df):
     
 
 def main():
-    st.title("Classificação de Itens para ME/EPP e Ampla Concorrência 3.2")
+    st.title("Classificação de Itens para ME/EPP e Ampla Concorrência 3.1")
 
     # Upload do arquivo Excel
     uploaded_file = st.file_uploader(
@@ -50,22 +49,6 @@ def main():
     # Carregar e exibir o arquivo .txt com as explicações
     instrucoes = carregar_instrucoes('como_usar.txt')  # Certifique-se de que o arquivo .txt está no diretório correto
     st.text(instrucoes)
-
-    # Gerar a planilha de exemplo
-    df = gerar_planilha_exemplo()
-
-    # Botão para download da planilha .xlsx
-    st.download_button(
-        label="Baixar Planilha de Exemplo",
-        data=df.to_excel(index=False, engine='openpyxl'),  # Salva o DataFrame em formato Excel
-        file_name='planilha_exemplo.xlsx',
-        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
-
-    # Se você quiser exibir a planilha na própria interface
-    st.write("Prévia da planilha:")
-    st.dataframe(df)
-
 
 if __name__ == '__main__':
     main()
